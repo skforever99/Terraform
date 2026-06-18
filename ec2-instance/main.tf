cbba3cc6ddecf02e57b71d1c5da1c0801edc618e
sunil kumar <sunilkumar@Mac.lan>
2026-06-18 15:34:06 +0530

    added variable file

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,7 +1,7 @@
 # generating key pair
 
 resource "aws_key_pair" "my-key" {
-  key_name = "terraform-ec2-key"
+  key_name   = "terraform-ec2-key"
   public_key = file("ec2-key.pub")
 }
 
@@ -16,25 +16,25 @@ resource "aws_default_vpc" "default" {
 # security groups
 
 resource "aws_security_group" "sg-1" {
-  name = "security-group-1"
+  name        = "security-group-1"
   description = " this is created by terraform"
-  vpc_id = aws_default_vpc.default.id  #interpolation = fetching values from other block
+  vpc_id      = aws_default_vpc.default.id #interpolation = fetching values from other block
   tags = {
     name = " terraform-sg "
   }
   # inbound rules
-  ingress  {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "ssh open"
   }
   # egress rules
-  egress  {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     description = "internet access allowed"
   }
@@ -42,10 +42,10 @@ resource "aws_security_group" "sg-1" {
 }
 
 resource "aws_instance" "sunil-ec2" {
-  key_name = aws_key_pair.my-key.key_name  
+  key_name        = aws_key_pair.my-key.key_name
   security_groups = [aws_security_group.sg-1.name]
-  ami           = "ami-0152204c1a187337c"
-  instance_type = "t3.micro"
+  ami             = var.ami
+  instance_type   = var.ec-2-instance-type
 
   tags = {
     Name = "terraform-ec2"
